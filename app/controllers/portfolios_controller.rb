acbc4e807630d53e4acf204b4d7a379f3f4e2218
John Higgins <johnhiggins@outlook.ie>
2019-08-01 12:40:09 +0100

    added portfolio index and new actions, added some styling to app and a bootstrap navbar

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,25 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolios = Portfolio.all
+  end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(
+      :title,
+      :subtitle,
+      :body,
+      :image_main,
+      :image_thumb
+    )
+  end
 end
